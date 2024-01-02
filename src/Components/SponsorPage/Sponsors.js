af9330032ce78cee75ec83e2251cac48c32fa53d
dipak-01 <imt_2022037@iiitm.ac.in>
2024-01-02 22:41:56 +0530

    Sponsors images added along with the details

--- src/Components/SponsorPage/Sponsors.js
+++ src/Components/SponsorPage/Sponsors.js
@@ -3,6 +3,7 @@ import './Sponsors.css';
 import cocaImg from '../../assets/cocacola.60789f99.png'
 import img2 from '../../assets/blu-lounge.f54b1da0.jpg'
 import Navbar from '../Navbar/Navbar';
+ 
 
 const Sponsors = () => {
   return (
@@ -31,7 +32,7 @@ const Sponsors = () => {
           <div className="sponsors-titleSpon">
             <div className="sponsors-sponCard">
               <div className="sponsors-imgContain">
-                <a href=""><img src={cocaImg} alt="" /></a>
+                <a href=""><img src='./img/cocacola.60789f99.png' alt="" /></a>
               </div>
               <div className="sponsors-sponInfo">
                 <h4>Coca Cola</h4>
@@ -42,83 +43,173 @@ const Sponsors = () => {
           <div className="sponsors-restSpon">
             <div className="sponsors-sponCard">
               <div className="sponsors-imgContain">
-                <a href=""><img src={cocaImg} alt="" /></a>
+                <a href=""><img src='./img/tribevibe.3e919435.png' alt="" /></a>
               </div>
               <div className="sponsors-sponInfo">
-                <h4>SPONSOR NAME</h4>
-                <span>Lorem ipsum dolor.</span>
+                <h4>TribeVibe</h4>
+                <span>Festival Partner</span>
               </div>
             </div>
             <div className="sponsors-sponCard">
               <div className="sponsors-imgContain">
-                <a href=""><img src={img2} alt="" /></a>
+                <a href=""><img src='./img/blu-lounge.f54b1da0.jpg' alt="" /></a>
               </div>
               <div className="sponsors-sponInfo">
-                <h4>SPONSOR NAME</h4>
-                <span>Lorem ipsum dolor.</span>
+                <h4>Blu Lounge</h4>
+                <span>Flagship Event Sponsor - Corna </span>
               </div>
             </div>
             <div className="sponsors-sponCard">
               <div className="sponsors-imgContain">
-                <a href=""><img src={img2} alt="" /></a>
+                <a href=""><img src='./img/yono-sbi.30ffda32.png' alt="" /></a>
               </div>
               <div className="sponsors-sponInfo">
-                <h4>SPONSOR NAME</h4>
-                <span>Lorem ipsum dolor.</span>
+                <h4>YONO - SBI</h4>
+                <span>Flagship Event Sponsor - MnM </span>
               </div>
             </div>
             <div className="sponsors-sponCard">
               <div className="sponsors-imgContain">
-                <a href=""><img src={img2} alt="" /></a>
+                <a href=""><img src='./img/sics.4420f55c.jpg' alt="" /></a>
               </div>
               <div className="sponsors-sponInfo">
-                <h4>SPONSOR NAME</h4>
-                <span>Lorem ipsum dolor.</span>
+                <h4>SICS </h4>
+                <span>Event Sponsor - StepUp </span>
               </div>
             </div>
             <div className="sponsors-sponCard">
               <div className="sponsors-imgContain">
-                <a href=""><img src={img2} alt="" /></a>
+                <a href=""><img src='./img/fastrack.3cf395af.png' alt="" /></a>
               </div>
               <div className="sponsors-sponInfo">
-                <h4>SPONSOR NAME</h4>
-                <span>Lorem ipsum dolor.</span>
+                <h4>Fastrack</h4>
+                <span>Title Sponsor - Parivesh </span>
               </div>
             </div>
             <div className="sponsors-sponCard">
               <div className="sponsors-imgContain">
-                <a href=""><img src={img2} alt="" /></a>
+                <a href=""><img src='./img/jawed-habib.d63228a3.png' alt="" /></a>
               </div>
               <div className="sponsors-sponInfo">
-                <h4>SPONSOR NAME</h4>
-                <span>Lorem ipsum dolor.</span>
+                <h4>Jawed Habib </h4>
+                <span>Flagship Event Co-Sponsor - Parivesh </span>
               </div>
             </div>
             <div className="sponsors-sponCard">
               <div className="sponsors-imgContain">
-                <a href=""><img src={img2} alt="" /></a>
+                <a href=""><img src='./img/bigfm.90230eb8.png' alt="" /></a>
               </div>
               <div className="sponsors-sponInfo">
-                <h4>SPONSOR NAME</h4>
-                <span>Lorem ipsum dolor.</span>
+                <h4>Big FM </h4>
+                <span>Event Sponsor - Morning Radio </span>
               </div>
             </div>
             <div className="sponsors-sponCard">
               <div className="sponsors-imgContain">
-                <a href=""><img src={img2} alt="" /></a>
+                <a href=""><img src='./img/inox.d5254ef0.png' alt="" /></a>
               </div>
               <div className="sponsors-sponInfo">
-                <h4>SPONSOR NAME</h4>
-                <span>Lorem ipsum dolor.</span>
+                <h4>INOX</h4>
+                <span>Cinema Partner </span>
               </div>
             </div>
             <div className="sponsors-sponCard">
               <div className="sponsors-imgContain">
-                <a href=""><img src={cocaImg} alt="" /></a>
+                <a href=""><img src='./img/pnb.eb2acf62.png' alt="" /></a>
               </div>
               <div className="sponsors-sponInfo">
-                <h4>SPONSOR NAME</h4>
-                <span>Lorem ipsum dolor.</span>
+                <h4>PNB</h4>
+                <span>Banking Partner</span>
+              </div>
+            </div>
+            <div className="sponsors-sponCard">
+              <div className="sponsors-imgContain">
+                <a href=""><img src='./img/sehatone.a1081264.png' alt="" /></a>
+              </div>
+              <div className="sponsors-sponInfo">
+                <h4>SehatOne </h4>
+                <span>HealthCare Partner</span>
+              </div>
+            </div>
+            <div className="sponsors-sponCard">
+              <div className="sponsors-imgContain">
+                <a href=""><img src='./img/dalmia.2cf8a5a7.png' alt="" /></a>
+              </div>
+              <div className="sponsors-sponInfo">
+                <h4>Dalmia Group</h4>
+                <span>Foundation Partner</span>
+              </div>
+            </div>
+            <div className="sponsors-sponCard">
+              <div className="sponsors-imgContain">
+                <a href=""><img src='./img/bigfm.90230eb8.png' alt="" /></a>
+              </div>
+              <div className="sponsors-sponInfo">
+                <h4>Big FM</h4>
+                <span>Radio Partner</span>
+              </div>
+            </div>
+            <div className="sponsors-sponCard">
+              <div className="sponsors-imgContain">
+                <a href=""><img src='./img/boi.dd87a364.png' alt="" /></a>
+              </div>
+              <div className="sponsors-sponInfo">
+                <h4>BOI</h4>
+                <span>Banking Partner</span>
+              </div>
+            </div>
+            <div className="sponsors-sponCard">
+              <div className="sponsors-imgContain">
+                <a href=""><img src='./img/clarks.d705d471.jpg' alt="" /></a>
+              </div>
+              <div className="sponsors-sponInfo">
+                <h4>Clarks</h4>
+                <span>Stay Partner</span>
+              </div>
+            </div>
+            <div className="sponsors-sponCard">
+              <div className="sponsors-imgContain">
+                <a href=""><img src='./img/india-khabar.7b9841b3.jpeg' alt="" /></a>
+              </div>
+              <div className="sponsors-sponInfo">
+                <h4>India Khabar</h4>
+                <span>Streaming Partner</span>
+              </div>
+            </div>
+            <div className="sponsors-sponCard">
+              <div className="sponsors-imgContain">
+                <a href=""><img src='./img/nisha-dance.e75dd42b.jpg' alt="" /></a>
+              </div>
+              <div className="sponsors-sponInfo">
+                <h4>Nisha Dance Academy</h4>
+                <span>Event Co-Sponsor - Step Up</span>
+              </div>
+            </div>
+            <div className="sponsors-sponCard">
+              <div className="sponsors-imgContain">
+                <a href=""><img src='./img/eurofins.71c92ef1.jpg' alt="" /></a>
+              </div>
+              <div className="sponsors-sponInfo">
+                <h4>Eurofins Clinical Genetics</h4>
+                <span>Sponsor</span>
+              </div>
+            </div>
+            <div className="sponsors-sponCard">
+              <div className="sponsors-imgContain">
+                <a href=""><img src='./img/atkt.605e2d33.png' alt="" /></a>
+              </div>
+              <div className="sponsors-sponInfo">
+                <h4>ATKT</h4>
+                <span>Sponsor</span>
+              </div>
+            </div>
+            <div className="sponsors-sponCard">
+              <div className="sponsors-imgContain">
+                <a href=""><img src='./img/o-genie.6db32bfd.jpg' alt="" /></a>
+              </div>
+              <div className="sponsors-sponInfo">
+                <h4>O Genie</h4>
+                <span>Sponsor</span>
               </div>
             </div>
           </div>
@@ -128,29 +219,29 @@ const Sponsors = () => {
           <div className="sponsors-restSpon">
             <div className="sponsors-sponCard">
               <div className="sponsors-imgContain">
-                <a href=""><img src={img2} alt="" /></a>
+                <a href=""><img src='./img/punhub.8f11d963.png'  alt="" /></a>
               </div>
               <div className="sponsors-sponInfo">
-                <h4>SPONSOR NAME</h4>
-                <span>Lorem ipsum dolor.</span>
+                <h4>PunHub </h4>
+                <span>Social Media Partner </span>
               </div>
             </div>
             <div className="sponsors-sponCard">
               <div className="sponsors-imgContain">
-                <a href=""><img src={img2} alt="" /></a>
+                <a href=""><img src='./img/gwaliorMP.865ea6ad.png' alt="" /></a>
               </div>
               <div className="sponsors-sponInfo">
-                <h4>SPONSOR NAME</h4>
-                <span>Lorem ipsum dolor.</span>
+                <h4>GwaliorMPIndia </h4>
+                <span>Social Media Partner </span>
               </div>
             </div>
             <div className="sponsors-sponCard">
               <div className="sponsors-imgContain">
-                <a href=""><img src={img2} alt="" /></a>
+                <a href=""><img src='./img/city-vibes.e2259393.png' alt="" /></a>
               </div>
               <div className="sponsors-sponInfo">
-                <h4>SPONSOR NAME</h4>
-                <span>Lorem ipsum dolor.</span>
+                <h4>The City Vibes Gwalior </h4>
+                <span>Social Media Partner </span>
               </div>
             </div>
           </div>
