a6c7a8ba94c83824f8d70d859367a82a913b2f25
Mrigank <108195614+mrigank-45@users.noreply.github.com>
2024-01-10 21:33:43 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,35 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import Home from "./Components/HomePage/Home";
-import SchedulePage from "./Components/SchedulePage/SchedulePage";
-import Sponsors from "./Components/SponsorPage/Sponsors";
-import Pronites from "./Components/Pronites/Pronites";
-import AboutPage from "./Components/AboutPage/AboutPage";
-import EventPage from "./Components/EventPage/EventPage";
-import Contact from "./Components/Contact/Contact";
-import Faq from "./Components/Faq/Faq";
 import Login from "./Components/Login/Login";
 import Signup from "./Components/Signup/Signup";
-import Pronites2 from "./Components/Pronites2/Pronites2";
-import Navbar from "./Components/Navbar/Navbar";
-import Team from "./Components/Team/Team";
+
 
 function App() {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<Home />} />
-        {/* <Route path='/schedule' element={<SchedulePage/>}/> */}
-        <Route path="/sponsors" element={<Sponsors />} />
-        {/* <Route path='/pronites' element={<Pronites/>}/> */}
-        <Route path="/pronites" element={<Pronites2 />} />
-        <Route path="/about" element={<AboutPage />} />
-        <Route path="/events" element={<EventPage />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/faq" element={<Faq />} />
-        {/* <Route path="/login" element={<Login />} /> */}
-        <Route path="/register" element={<Signup />} />
-        <Route path="/team" element={<Team />} />
+        <Route path="/" element={<Signup />} />
       </Routes>
     </div>
   );
