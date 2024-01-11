b76fd3d309005e9ad8ee17ccd2086c5ec0a31bca
Mrigank <108195614+mrigank-45@users.noreply.github.com>
2024-01-11 15:56:14 +0530

    show step changed

--- src/Components/Signup/Signup.jsx
+++ src/Components/Signup/Signup.jsx
@@ -133,6 +133,10 @@ const Signup = () => {
       );
       console.log(response.data);
 
+      setFormData(initialFormData);
+      setShowStep1(true);
+      setShowStep2(false);
+
       setMessege("Registraion Successful");
       setShowToast(true);
     } catch (e) {
