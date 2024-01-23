5866ebd95067c05a8921bc882244080387fa0334
UJJWAL JAIN <ujjwaljain@UJJWALs-MacBook-Air.local>
2024-01-23 17:22:59 +0530

    integrated with backend

--- src/Components/CaDashboard/Profile/Profile.jsx
+++ src/Components/CaDashboard/Profile/Profile.jsx
@@ -2,50 +2,71 @@ import { Stack } from "@mui/material";
 import "./Profile.css";
 
 const Profile = () => {
+  // const [loading, setLoading] = useState(false)
+  // const response = await axios.get("http://localhost:3000/getCaData")
+  // const ca = response.data
+  // const registrations = ca.referrals.length;
+  // let progress;
+  // if (registrations >= 15) {
+  //   progress = 100;
+  // } else {
+  //   progress = (registrations * 100) / 15;
+  // }
+
   return (
     <div className="profile">
       <div className="detail-section">
         <Stack spacing={4}>
-
           <h4 className="data data-first">
-            Name :  Abc
+            Name : Abc
+            {/* {ca.name}  */}
           </h4>
           <h4 className="data">
-            College :  IIITM
+            College : IIITM
+            {/* {ca.college} */}
           </h4>
           <h4 className="data">
-            City :  Gwalior
+            City : Gwalior
+            {/* {ca.city} */}
           </h4>
           <h4 className="data">
             Gender : Male
+            {/* {ca.gender} */}
           </h4>
 
           <h4 className="data">
             Email : abc@gmail.com
+            {/* {ca.email} */}
           </h4>
           <h4 className="data">
-            Phone No :  1223344556
+            Phone No : 1223344556
+            {/* {ca.phone} */}
           </h4>
 
           <h4 className="data">
-            DOB :  2004
+            DOB : 2003
+            {/* {ca.dob} */}
           </h4>
           <h4 className="data">
-            Referral Code :  12345
+            Referral Code : 12345
+            {/* {ca.referralCode} */}
           </h4>
           <h4 className="data">
-            Registrations :     12
+            Registrations : 12
+            {/* {registrations} */}
           </h4>
           <h4 className="data">
             Credits : 100
+            {/* {registrations * 100} */}
           </h4>
           <h4 className="data">
             Progress : 12
+            {/* {progress} */}
           </h4>
           <h4 className="data data-last">
-            **Note : If you are successful to complete 15 referrals, you will avail 50% discount.
+            **Note : If you are successful to complete 15 referrals, you will
+            avail 50% discount.
           </h4>
-
         </Stack>
       </div>
     </div>
--- src/Components/CaDashboard/Table/Table.jsx
+++ src/Components/CaDashboard/Table/Table.jsx
@@ -1,78 +1,59 @@
 import * as React from "react";
-import { useMemo } from 'react';
+import { useMemo } from "react";
 import {
   MaterialReactTable,
   useMaterialReactTable,
-} from 'material-react-table';
+} from "material-react-table";
 import "./Table.css";
 import { ThemeProvider, createTheme } from "@mui/material";
 
 const data = [
   {
-    name: {
-      firstName: 'John',
-      lastName: 'Doe',
-    },
-    address: '261 Erdman Ford',
-    city: 'East Daphne',
-    state: 'Kentucky',
-  },
-  {
-    name: {
-      firstName: 'Jane',
-      lastName: 'Doe',
-    },
-    address: '769 Dominic Grove',
-    city: 'Columbus',
-    state: 'Ohio',
-  },
-  {
-    name: {
-      firstName: 'Joe',
-      lastName: 'Doe',
-    },
-    address: '566 Brakus Inlet',
-    city: 'South Linda',
-    state: 'West Virginia',
-  },
-  {
-    name: {
-      firstName: 'Kevin',
-      lastName: 'Vandy',
-    },
-    address: '722 Emie Stream',
-    city: 'Lincoln',
-    state: 'Nebraska',
-  },
-  {
-    name: {
-      firstName: 'Joshua',
-      lastName: 'Rolluffs',
-    },
-    address: '32188 Larkin Turnpike',
-    city: 'Charleston',
-    state: 'South Carolina',
+    name: "John",
+    email: "john@example.com",
+    phone: 1234567890,
+    gender: "Male",
+    college: "Example College",
+    city: "Example City",
+    dob: "2000-01-01T00:00:00.000Z",
+    referralCode: "ABC123",
+    createdAt: "2022-01-01T00:00:00.000Z",
+    updatedAt: "2022-01-02T00:00:00.000Z",
+    referrals: [
+      {
+        name: "Referral 1",
+        email: "referral1@example.com",
+        phone: 1234567899,
+        college: "Example College",
+      },
+      {
+        name: "Referral 2",
+        email: "referral2@example.com",
+        phone: 1234567898,
+        college: "Example College",
+      },
+
+    ],
   },
 ];
 
+// const response = await axios.get("http://localhost:3000/getCaData")
+// const ca = response.data
+// console.log(ca);
+// const referrals = ca.referrals
 
-  // const response = await axios.get("http://localhost:3000/getCaData")
-    // const ca = response.data
-    // console.log(ca);
-    // const referrals = ca.referrals
-
- const darkTheme = createTheme({
+const darkTheme = createTheme({
   palette: {
-    mode: 'dark',
+    mode: "dark",
     primary: {
-      main: '#424242', // Adjust the color to a lighter shade
+      main: "#424242", // Adjust the color to a lighter shade
     },
     background: {
-      default: '#1e1e1e6f', // Adjust the background color to a lighter shade
-      paper: '#424242', // Adjust the paper color to a lighter shade
+      default: "#1e1e1e6f", // Adjust the background color to a lighter shade
+      paper: "#424242", // Adjust the paper color to a lighter shade
     },
     table: {
-      background: '#555', // Adjust the table background color to a lighter shade
+      background: "#555", // Adjust the table background color to a lighter shade
     },
   },
 });
@@ -80,80 +61,101 @@ const data = [
 export default function BasicTable() {
   const theme = createTheme({
     palette: {
-      mode: 'dark'
-    }
-  })
+      mode: "dark",
+    },
+  });
 
   const columns = useMemo(
     () => [
       {
-        accessorKey: 'name.firstName', //access nested data with dot notation
-        header: 'First Name',
+        accessorKey: "name", //access nested data with dot notation
+        header: "Name",
         size: 150,
       },
       {
-        accessorKey: 'name.lastName',
-        header: 'Last Name',
-        size: 150,
-      },
-      {
-        accessorKey: 'address', //normal accessorKey
-        header: 'Address',
+        accessorKey: "email",
+        header: "Email",
         size: 200,
       },
       {
-        accessorKey: 'city',
-        header: 'City',
-        size: 150,
+        accessorKey: "phone",
+        header: "Phone",
+        size: 200,
       },
       {
-        accessorKey: 'state',
-        header: 'State',
-        size: 150,
+        accessorKey: "college",
+        header: "College",
+        size: 200,
       },
+      // Add more columns as needed
     ],
-    [],
+    []
   );
 
+  const combinedColumns = [...columns];
+
+  const tableData = useMemo(() => {
+    const flattenedData = data.flatMap((item) => {
+      return item.referrals.map((referral) => ({
+        ...item,
+        referrals: undefined,
+        ...referral,
+      }));
+    });
+
+    return flattenedData;
+  }, [data]);
+
+  const [pagination, setPagination] = React.useState({
+    pageIndex: 0,
+    pageSize: 5, //customize the default page size
+  });
+  
+  React.useEffect(() => {
+    //do something when the pagination state changes
+  }, [pagination.pageIndex, pagination.pageSize]);
+  
+
   const table = useMaterialReactTable({
-    columns,
-    data,
+    columns: combinedColumns,
+    data: tableData,
+    onPaginationChange: setPagination, //hoist pagination state to your state when it changes internally
+    state: { pagination },
     enableFullScreenToggle: false,
     visibleInShowHideMenu: false,
     muiTableProps: {
       sx: {
-        border: '1px solid rgba(81, 81, 81, .5)',
-        color:"white"
+        border: "1px solid rgba(81, 81, 81, .5)",
+        color: "white",
       },
     },
     muiTableHeadCellProps: {
       sx: {
-        border: '1px solid rgba(81, 81, 81, .5)',
-        color:"white",
-        fontStyle: 'italic',
-        fontWeight: 'normal',
-        fontFamily: 'monospace',
+        border: "1px solid rgba(81, 81, 81, .5)",
+        color: "white",
+        fontStyle: "italic",
+        fontWeight: "normal",
+        fontFamily: "monospace",
       },
     },
     muiTableBodyCellProps: {
       sx: {
-        border: '1px solid rgba(81, 81, 81, .5)',
-        color:"white",
-        fontFamily: 'monospace', 
+        border: "1px solid rgba(81, 81, 81, .5)",
+        color: "white",
+        fontFamily: "monospace",
       },
     },
-     //data must be memoized or stable (useState, useMemo, defined outside of this component, etc.)
-     muiPaginationProps: {
-      color: 'secondary',
-      rowsPerPageOptions: [5, 10, 20],
-      shape: 'rounded',
-      variant: 'outlined',
- 
+    //data must be memoized or stable (useState, useMemo, defined outside of this component, etc.)
+    muiPaginationProps: {
+      color: "secondary",
+      rowsPerPageOptions: [5],
+      shape: "rounded",
+      variant: "outlined",
     },
-    });
+  });
 
   return (
-   <ThemeProvider theme={darkTheme}>
+    <ThemeProvider theme={darkTheme}>
       <div className="Table overflow-x-auto">
         <MaterialReactTable table={table} />
       </div>
