6e9981d9355856080d2e912314ffbaba3f31fe07
Mrigank <108195614+mrigank-45@users.noreply.github.com>
2024-02-03 02:26:56 +0530

    new events

--- src/Components/Dashboard/EventsPage/EventCardsLayout.js
+++ src/Components/Dashboard/EventsPage/EventCardsLayout.js
@@ -9,7 +9,7 @@ function EventCardsLayout() {
         <h3 style={{
           display: "flex", justifyContent: "center", alignItems: "center", fontWeight: "900",
           marginLeft: "-25%",
-        }}  >La Arte Fiesta</h3>
+        }}  >Le Arte Fiesta</h3>
         <div className=" mt-2 flex flex-col ml-[-10%] sm:ml-0 sm:grid sm:grid-cols-3 lg:grid-cols-6 sm:gap-x-16 pt-6 sm:pt-[25vh] lg:pt-6 mr-[10%] lg:mr-0" >
           {events?.map((details, index) => {
             return (details.Category == "La Arte Fiesta" && < Card data={details} key={index} className="flex justify-center" />)
@@ -26,6 +26,27 @@ function EventCardsLayout() {
 
           {events?.map((details, index) => {
             return (details.Category == "Step-up" && < Card data={details} key={index} className="flex justify-center" />)
+          })}
+        </div>
+
+        <h3 style={{
+          display: "flex", justifyContent: "center", alignItems: "center", fontWeight: "900",
+          marginLeft: "-25%"
+        }} className=" mt-4 " >Literary arts</h3>
+        <div className=" mt-2 flex flex-col  ml-[-10%] sm:ml-0  sm:grid sm:grid-cols-3 lg:grid-cols-6 sm:gap-x-16 pt-6 sm:pt-[25vh] lg:pt-6 mr-[10%] lg:mr-0">
+
+          {events?.map((details, index) => {
+            return (details.Category == "Literary_SpeakingArts" && < Card data={details} key={index} className="flex justify-center" />)
+          })}
+        </div>
+
+        <h3 style={{
+          display: "flex", justifyContent: "center", alignItems: "center", fontWeight: "900",
+          marginLeft: "-25%",
+        }}  >The Pikture</h3>
+        <div className=" mt-2 flex flex-col ml-[-10%] sm:ml-0 sm:grid sm:grid-cols-3 lg:grid-cols-6 sm:gap-x-16 pt-6 sm:pt-[25vh] lg:pt-6 mr-[10%] lg:mr-0" >
+          {events?.map((details, index) => {
+            return (details.Category == "ThePikture" && < Card data={details} key={index} className="flex justify-center" />)
           })}
         </div>
 
