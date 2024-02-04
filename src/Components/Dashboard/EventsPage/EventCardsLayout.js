9ef9a69bacaec9cddb0e49b572587e404260a69a
Mrigank <108195614+mrigank-45@users.noreply.github.com>
2024-02-04 17:53:18 +0530

    eumilia event

--- src/Components/Dashboard/EventsPage/EventCardsLayout.js
+++ src/Components/Dashboard/EventsPage/EventCardsLayout.js
@@ -32,6 +32,19 @@ function EventCardsLayout() {
         <h3 style={{
           display: "flex", justifyContent: "center", alignItems: "center", fontWeight: "900",
           marginLeft: "-25%"
+        }} className=" mt-4 " >Eumelia</h3>
+        <div className=" mt-2 flex flex-col  ml-[-10%] sm:ml-0  sm:grid sm:grid-cols-3 lg:grid-cols-6 sm:gap-x-16 pt-6 sm:pt-[25vh] lg:pt-6 mr-[10%] lg:mr-0">
+
+
+
+          {events?.map((details, index) => {
+            return (details.Category == "Eumelia" && < Card data={details} key={index} className="flex justify-center" />)
+          })}
+        </div>
+
+        <h3 style={{
+          display: "flex", justifyContent: "center", alignItems: "center", fontWeight: "900",
+          marginLeft: "-25%"
         }} className=" mt-4 " >Literary arts</h3>
         <div className=" mt-2 flex flex-col  ml-[-10%] sm:ml-0  sm:grid sm:grid-cols-3 lg:grid-cols-6 sm:gap-x-16 pt-6 sm:pt-[25vh] lg:pt-6 mr-[10%] lg:mr-0">
 
