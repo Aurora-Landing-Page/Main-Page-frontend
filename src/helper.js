5242582a08a6a4e709e2d6ec1935dfec83e89fd8
UJJWAL JAIN <ujjwaljain@UJJWALs-MacBook-Air.local>
2024-02-08 00:51:53 +0530

    Forget Password implemented

--- src/helper.js
+++ src/helper.js
@@ -1,3 +1,3 @@
-// const BACKEND_URL = "https://api.aurorafest.in";
-const BACKEND_URL = "http://localhost:3000";
+const BACKEND_URL = "https://api.aurorafest.in";
+// const BACKEND_URL = "http://localhost:3000";
 export default BACKEND_URL;
