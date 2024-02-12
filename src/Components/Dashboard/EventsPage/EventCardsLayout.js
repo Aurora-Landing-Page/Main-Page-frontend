f4d5bd46bfd75dd432586ddd538cca7459ca98b2
Mrigank <108195614+mrigank-45@users.noreply.github.com>
2024-02-12 20:47:18 +0530

    event order

--- src/Components/Dashboard/EventsPage/EventCardsLayout.js
+++ src/Components/Dashboard/EventsPage/EventCardsLayout.js
@@ -6,15 +6,7 @@ function EventCardsLayout() {
   return (
     <>
       <div className=" mt-4 flex flex-col" >
-        <h3 style={{
-          display: "flex", justifyContent: "center", alignItems: "center", fontWeight: "900",
-          marginLeft: "-25%",
-        }}  >Le Arte Fiesta</h3>
-        <div className=" mt-2 flex flex-col ml-[-10%] sm:ml-0 sm:grid sm:grid-cols-3 lg:grid-cols-6 sm:gap-x-16 pt-6 sm:pt-[25vh] lg:pt-6 mr-[10%] lg:mr-0" >
-          {events?.map((details, index) => {
-            return (details.Category == "La Arte Fiesta" && < Card data={details} key={index} className="flex justify-center" />)
-          })}
-        </div>
+        
 
         <h3 style={{
           display: "flex", justifyContent: "center", alignItems: "center", fontWeight: "900",
@@ -50,6 +42,16 @@ function EventCardsLayout() {
 
           {events?.map((details, index) => {
             return (details.Category == "Literary_SpeakingArts" && < Card data={details} key={index} className="flex justify-center" />)
+          })}
+        </div>
+
+        <h3 style={{
+          display: "flex", justifyContent: "center", alignItems: "center", fontWeight: "900",
+          marginLeft: "-25%",
+        }}  >Le Arte Fiesta</h3>
+        <div className=" mt-2 flex flex-col ml-[-10%] sm:ml-0 sm:grid sm:grid-cols-3 lg:grid-cols-6 sm:gap-x-16 pt-6 sm:pt-[25vh] lg:pt-6 mr-[10%] lg:mr-0" >
+          {events?.map((details, index) => {
+            return (details.Category == "La Arte Fiesta" && < Card data={details} key={index} className="flex justify-center" />)
           })}
         </div>
 
